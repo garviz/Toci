e68ba73911b1cde2866863f1b55f912e4e785710
Julio Cesar Garcia Vizcaino <garviz@garviz.mx>
2012-11-16 15:03:52 +0100

    Scheduling OpenMP tests

--- experiments.sh
+++ experiments.sh
@@ -19,36 +19,14 @@
 
 set -o nounset                              # Treat unset variables as an error
 
-export OMP_NUM_THREADS=24
 export length=20
 export ref=`basename $1`
 export qry=`basename $2`
 
-for chunks in {1..128}
+for thrd in {1..24}
 do
-    export OMP_SCHEDULE="$3,$chunks"
-    likwid-perfctr -C 0-1 -g FLOPS_DP mpirun -np 1 ./toci -C $chunks -mum -l $length $1 $2 1> results/$(date +"%m-%d-%Y_%T")_"$ref"_"$qry"_"$OMP_SCHEDULE"_$chunks.dat 2>&1
-    echo $chunks
+    export OMP_SCHEDULE="$3,$5"
+    export OMP_NUM_THREADS=$thrd
+    likwid-perfctr -C 0-23 -g FLOPS_DP mpirun -np 1 ./toci -C $4 -mum -l $length $1 $2 1> results/$(date +"%m-%d-%Y_%T")_"$ref"_"$qry"_"$OMP_SCHEDULE"_$5.dat 2>&1
+    echo $thrd
 done
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
